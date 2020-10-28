6e62b71ada6704889277b4f81b621cc78f15811d
ajbain <ashleytiller@gmail.com>
2020-10-28 16:19:31 -0400

    committing 2nd challenge to gitHub

---
+++ ChallengeSix_Repository/Vehicle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeSix_Repository
+{
+    public enum VehicleType { Gas, Hybrid, Electric}
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public double Mileage { get; set; }
+        public VehicleType VehicleType { get; set; }
+        public bool isGreen
+        {
+            get
+            {
+                switch (VehicleType)
+                {
+                    case VehicleType.Hybrid:
+                    case VehicleType.Electric:
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+        }
+        public Vehicle() { }
+        public Vehicle( string make, string model, string year, double mileage, VehicleType vehicleType)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Mileage = mileage;
+            VehicleType = vehicleType;
+        }
+
+    }
+}
---
+++ ChallengeSix_Repository/Vehicle_Repo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeSix_Repository
+{
+    class Vehicle_Repo
+    {
+        private List<Vehicle> _vehicleDirectory = new List<Vehicle>();
+        //crud methods
+
+        //CREATE
+        public bool AddVehicle(Vehicle vehicle)
+        {
+            int startingCount = _vehicleDirectory.Count;
+            _vehicleDirectory.Add(vehicle);
+            bool wasAdded = (_vehicleDirectory.Count > startingCount) ? true : false;
+            return wasAdded;
+
+        }
+
+        //READ
+        public List<Vehicle> GetVehicles()
+        {
+            return _vehicleDirectory;
+        }
+
+        public Vehicle GetVehicleByMake(string make)
+        {
+            foreach (Vehicle vehicle in _vehicleDirectory)
+            {
+                if (vehicle.Make.ToLower() == make.ToLower())
+                {
+                    return vehicle;
+                }
+
+            }
+            return null;
+        }
+        //UPDATE
+
+        public bool UpdateVehicle(string make, Vehicle updatedVehicle)
+        {
+            Vehicle oldVehicle = GetVehicleByMake(make);
+            if (oldVehicle != null)
+            {
+                oldVehicle.Make = updatedVehicle.Make;
+                oldVehicle.Model = updatedVehicle.Model;
+                oldVehicle.Year = updatedVehicle.Year;
+                oldVehicle.Mileage = updatedVehicle.Mileage;
+                oldVehicle.VehicleType = updatedVehicle.VehicleType;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //DELETE
+
+        public bool DeleteVehicle(Vehicle existingVehicle)
+        {
+            bool deleteResult = _vehicleDirectory.Remove(existingVehicle);
+            return deleteResult;
+        }
+    }
+}
